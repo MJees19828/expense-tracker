0320afbb17cc4fb3f4253852d522308781f6d5a0
MJees19828 <101269780+MJees19828@users.noreply.github.com>
2022-08-12 14:58:36 +0000

    my expense-tragker

---
+++ app.js
@@ -0,0 +1,39 @@
+const form=document.getElementById("form-container");
+const items=document.getElementById("items");
+// function that will submit our data
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    let description=form.description.value
+    let amount =form.amount.value
+
+    generateHTML(description, amount)
+
+})
+// function that generate html stracture
+const generateHTML=(description, amount)=>{
+  
+  let HTML ; 
+   if(amount<0){
+HTML=`
+<li class="expense">
+
+<span>${description}</span>
+<span>${amount}</span>
+</li>
+
+`
+items.innerHTML+=HTML
+
+    }else{
+        HTML=`
+<li class="income">
+
+<span>${description}</span>
+<span>${amount}</span>
+</li>
+
+`
+items.innerHTML+=HTML
+
+    }
+}
